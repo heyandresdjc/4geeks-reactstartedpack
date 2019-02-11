a655466bf48aebd4832880fefad936591b2d6893
Andres De Jesus Cabrera <ajc2101@gmail.com>
2019-02-11 10:35:39 -0500

    firt deployment

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM node:11-alpine
+WORKDIR /usr/src/app
+COPY . .
+RUN npm install --silent
--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./app.css"
+import logo from "./image.png"
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <div className="app">
+          <h1>
+            Welcome to <br/>4Geeks Academy
+          </h1>
+          <p>This is a landing example to setup your development enverioment, for the course.</p>
+          <p>Go ahead and edit the <pre>src/App.js</pre></p>
+          <p>and the changes will reflect automatically.</p>
+        </div>
+      </React.Fragment>
     );
   }
 }
